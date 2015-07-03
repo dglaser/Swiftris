6d5d33a9c47f9c5f345a9d3932830fb4a9d88a64
Dale Glaser <dale.glaser@gmail.com>
2015-07-03 16:01:53 -0500

    first stopping point

---
+++ Swiftris/Array2D.swift
@@ -0,0 +1,30 @@
+//
+//  Array2D.swift
+//  Swiftris
+//
+//  Created by dale on 7/3/15.
+//  Copyright (c) 2015 Glaser. All rights reserved.
+//
+
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+    
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        
+        array = Array<T?>(count:rows * columns, repeatedValue: nil)
+    }
+    
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[(row * columns) + column]
+        }
+        set(newValue) {
+            array[(row * columns) + column] = newValue
+        }
+    }
+}
